10f8abc17ea6cb8cd6dea2fc1e0e8ee5307d08a4
Daniel Tavares <nieltvrs@gmail.com>
2023-08-23 14:13:01 -0300

    Update settings.py

--- debike/debike/settings.py
+++ debike/debike/settings.py
@@ -80,13 +80,17 @@ WSGI_APPLICATION = 'debike.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
+"""     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'debike',
         'USER': 'postgres',
         'PASSWORD': 'admin',
         'HOST': 'localhost',
         'PORT': '5432',
+    } """
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
