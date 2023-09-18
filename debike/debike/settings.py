5ae1fa95713331e88d5669490e0532158dda86ac
Daniel Tavares <nieltvrs@gmail.com>
2023-09-18 09:13:29 -0300

    Correção da issue#1

--- debike/debike/settings.py
+++ debike/debike/settings.py
@@ -80,14 +80,6 @@ WSGI_APPLICATION = 'debike.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-"""     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'debike',
-        'USER': 'postgres',
-        'PASSWORD': 'admin',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    } """
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
--- debike/templates/static/js/verify.js
+++ debike/templates/static/js/verify.js
@@ -1,7 +1,32 @@
 function youreSure() {
-	if (confirm("Você tem certeza?")) {
-		return true;
-	} else {
-		return false;
-	}
+if (confirm("Você tem certeza?")) {
+  return true;
+} else {
+  return false;
 }
+}
+
+function toggleOptions(bikeID) {
+  const bikeArticle = document.querySelector(`.bikes[value="${bikeID}"]`);
+  const opcoesArticle = document.querySelector(`.opcoes[value="${bikeID}"]`);
+
+  if (!bikeArticle.classList.contains('hidden')) {
+    bikeArticle.classList.add('hidden');
+    opcoesArticle.classList.remove('hidden');
+  } else {
+    opcoesArticle.classList.add('hidden');
+    bikeArticle.classList.remove('hidden');
+  }
+}
+
+const bikeButtons = document.querySelectorAll('.button-light#bike-button');
+bikeButtons.forEach((button) => {
+  const bikeID = button.parentElement.getAttribute('value')
+  button.addEventListener('click', () => toggleOptions(bikeID));
+});
+
+const opcoesButtons = document.querySelectorAll('.button-light#opcoes-button');
+opcoesButtons.forEach((button) => {
+  const bikeID = button.parentElement.getAttribute('value');
+  button.addEventListener('click', () => toggleOptions(bikeID));
+});
